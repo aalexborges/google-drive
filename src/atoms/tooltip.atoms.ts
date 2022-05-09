5ba92ef8f7fd6d29e891403a0123c73fd697c856
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-05-09 14:42:11 -0300

    Removed Recoil

--- src/atoms/tooltip.atoms.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil'
-
-export const tooltipState = atom({
-  key: 'tooltipState',
-  default: '',
-})
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RecoilRoot } from 'recoil'
 import { ThemeProvider } from 'styled-components'
 import App from './App'
 import './index.css'
@@ -9,10 +8,8 @@ import theme from './theme/theme'
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
